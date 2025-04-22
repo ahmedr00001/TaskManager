d30b58bd76c63ebb96d1c1e888696dc440dc3f0f
MUSTAFA3LI <zoro09402@gmail.com>
2025-04-22 19:55:30 +0200

    Prisistant Data

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -99,29 +99,29 @@ WSGI_APPLICATION = 'TaskManager.wsgi.application'
 # FOR DEFAULT DATABASE
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # FOR DOCKER DATABASE
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'task_db',
-#         'USER': 'user',
-#         'PASSWORD': 'pass',
-#         'HOST': 'db',
-#         'PORT': '3306',  
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
 
 
 
