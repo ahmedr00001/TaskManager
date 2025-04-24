7d3236a3ba7e857836c2b9e10a884f9bd02d9665
MUSTAFA3LI <zoro09402@gmail.com>
2025-04-24 14:41:24 +0200

    Delete Volume

--- Terraform/AWS/7-security_groups.tf
+++ Terraform/AWS/7-security_groups.tf
@@ -82,3 +82,29 @@ resource "aws_security_group" "private_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.distritask_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
